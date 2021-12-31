161ae4b6c1489374e4564d0f2a6984ef52f5a5dd
Peter Hänsgen <peter@sunday-afternoon-computing.de>
2021-12-31 19:13:09 +0100

    Executables changed to bash script

--- git-remote-s3enc/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Generates a git-remote-s3enc executable in /usr/local/bin which starts the main class.
-#
-
-echo "#!/usr/bin/java -jar $HOME/.m2/repository/sunday/git-remote-s3enc/0.1-SNAPSHOT/git-remote-s3enc-0.1-SNAPSHOT.jar" > /usr/local/bin/git-remote-s3enc
-chmod 755 /usr/local/bin/git-remote-s3enc
